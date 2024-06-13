9ded0c5061085025af40ad99509fca284ab21b94
nehanth-reddy <pnehanthd@gmail.com>
2024-06-13 12:21:22 -0400

    replacing buffer with scanner

--- Demo.java
+++ Demo.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) throws IOException {
        System.out.println("enter a number");
-       InputStreamReader in = new InputStreamReader(System.in);
-       BufferedReader bf = new BufferedReader(in);
+       //InputStreamReader in = new InputStreamReader(System.in);
+       //BufferedReader bf = new BufferedReader(in);
 
-       int num = Integer.parseInt(bf.readLine());
+       Scanner sc = new Scanner(System.in);
+       int num = sc.nextInt();
        System.out.println(num);
  
 
