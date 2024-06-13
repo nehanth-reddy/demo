c0b760af4a0dfcb0e57dbc1d54f3b29c1e4519e3
nehanth-reddy <pnehanthd@gmail.com>
2024-06-13 13:01:02 -0400

    using finally without catch

--- Demo.java
+++ Demo.java
@@ -1,19 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Demo {
-    public static void main(String[] args) 
+    public static void main(String[] args) throws NumberFormatException, IOException 
     {
 
-        int j = 0;
-        int i = 0;
-        try {
-            j= 18/i;
-        }
-        catch(Exception e)
+        int num = 0;
+        BufferedReader br = null;
+        try 
         {
-            System.out.println("something went wrong");
+            InputStreamReader in = new InputStreamReader(System.in);
+            br = new BufferedReader(in);
+            num = Integer.parseInt(br.readLine());
+            System.out.println(num);
         }
+       
         finally {
-            System.out.println("bye");
+            br.close();
         }
     }
     
