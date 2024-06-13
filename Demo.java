3a3c656a88b30c391f395b713b5f3fd8f727c833
nehanth-reddy <pnehanthd@gmail.com>
2024-06-12 20:05:20 -0400

    new throws variable

--- Demo.java
+++ Demo.java
@@ -1,22 +1,21 @@
+class A{
+    public void show() throws ClassNotFoundException
+    {
+        Class.forName("Demo");
+        
+    }
+}
+
+
+
 public class Demo {
     public static void main(String[] args) {
-        int i=20;
-        int j=0;
-        try
-        {
-            j=18/i;
-        }
-        catch(ArithmeticException e) 
-        {
-            j=18/1;
-            System.out.println("that's the default mesage   ");
-        }
-        catch(Exception e)
-        {
-            System.out.println("something went wrong.." + e);
-        }
-        System.out.println(j);
-        System.out.println("bye");
+       A obj = new A();
+       try {
+        obj.show();
+    } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+    }
     }
     
 }
