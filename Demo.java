abaf82a351ad1024cef34584e82af3e2b3972a66
nehanth-reddy <pnehanthd@gmail.com>
2024-06-13 12:19:32 -0400

    buffer reader

--- Demo.java
+++ Demo.java
@@ -1,22 +1,19 @@
-class A{
-    public void show() throws ClassNotFoundException
-    {
-        Class.forName("Demo");
-        
-    }
-}
-
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Demo {
-    public static void main(String[] args) {
-       A obj = new A();
-       try {
-        obj.show();
-    } catch (ClassNotFoundException e) {
-        e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+       System.out.println("enter a number");
+       InputStreamReader in = new InputStreamReader(System.in);
+       BufferedReader bf = new BufferedReader(in);
+
+       int num = Integer.parseInt(bf.readLine());
+       System.out.println(num);
+ 
+
     }
-    }
+
     
 }
    
