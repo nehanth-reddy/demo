3c645b38f267250dc9deaf7005a1a0296e5f8fb1
nehanth-reddy <pnehanthd@gmail.com>
2024-06-13 12:45:01 -0400

    a final ending type of senario

--- Demo.java
+++ Demo.java
@@ -1,20 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Demo {
-    public static void main(String[] args) throws IOException {
-       System.out.println("enter a number");
-       //InputStreamReader in = new InputStreamReader(System.in);
-       //BufferedReader bf = new BufferedReader(in);
+    public static void main(String[] args) 
+    {
 
-       Scanner sc = new Scanner(System.in);
-       int num = sc.nextInt();
-       System.out.println(num);
- 
-
+        int j = 0;
+        int i = 0;
+        try {
+            j= 18/i;
+        }
+        catch(Exception e)
+        {
+            System.out.println("something went wrong");
+        }
+        finally {
+            System.out.println("bye");
+        }
     }
+    
 
     
 }
