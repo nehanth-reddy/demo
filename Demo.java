e57c4eadef4eac576cbf81940c8b7f16cb3ff4dc
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 16:16:35 -0400

    using stream to fetch values

--- Demo.java
+++ Demo.java
@@ -1,15 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class Demo {
     public static void main(String[] args) 
     {
         List<Integer> nums = Arrays.asList(4,5,6,7,8);
-        Consumer<Integer> con = n -> System.out.println(n);
+        Stream<Integer> s1 = nums.stream();
+        s1.forEach(n -> System.out.println(n));
 
-
-        nums.forEach(con);
         
     }
     
