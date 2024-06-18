0aec43941b1845908a4ad75e1122b393fc466fe1
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 16:33:28 -0400

    stream variable

--- Demo.java
+++ Demo.java
@@ -7,10 +7,12 @@ public class Demo {
     public static void main(String[] args) 
     {
         List<Integer> nums = Arrays.asList(4,5,6,7,8);
-        Stream<Integer> s1 = nums.stream();
-        s1.forEach(n -> System.out.println(n));
+        int result = nums.stream()
+                        .filter(n -> n%2==0)
+                        .map(n -> n*2)
+                        .reduce(0, (c,e) -> c+e);
 
-        
+        System.out.println(result);
     }
     
 }
