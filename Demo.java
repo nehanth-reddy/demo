2755bce6e4c3998fbc4933db9e2c55b8dd5a5f3c
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 16:12:14 -0400

    using consumer interface to fetch numbers

--- Demo.java
+++ Demo.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Demo {
     public static void main(String[] args) 
     {
         List<Integer> nums = Arrays.asList(4,5,6,7,8);
+        Consumer<Integer> con = n -> System.out.println(n);
 
-        nums.forEach(n -> System.out.println(n));
+
+        nums.forEach(con);
         
     }
     
