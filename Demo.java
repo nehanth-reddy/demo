5aba028f3cc6ff2c655f2fa4e8fdcfeb42836141
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 15:09:30 -0400

    filtering numbers

--- Demo.java
+++ Demo.java
@@ -1,46 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
-
-
-class Student
-{
-    int age;
-    String name;
-
-    
-    public Student(int age, String name) {
-        this.age = age;
-        this.name = name;
-    }
-    
-    public String toString() {
-        return "Student [age=" + age + ", name=" + name + "]";
-    }
-
-}
-
-
-
-
 
 public class Demo {
     public static void main(String[] args) 
     {
+        List<Integer> nums = Arrays.asList(4,5,6,7,8);
 
-        Comparator<Student> com = (i, j) -> i.age > j.age?1:-1;
-            
-       List<Student> studs = new ArrayList<>();
-       studs.add(new Student(25, "nick"));
-       studs.add(new Student(65, "nehanth"));
-       studs.add(new Student(13, "alex"));
-       studs.add(new Student(14, "tom"));
+        int sum = 0;
+        for(int n : nums)
+        {
+            if(n%2==0)
+            {
+                n = n*2;
+                sum = sum + n;
+            }
+        }
 
-       Collections.sort(studs, com);
-       for(Student s : studs)
-            System.out.println(s);
-       
+        System.out.println(sum);
     }
     
 }
