82b0965b001904f4a2113e92089b5f381c0d9100
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 14:04:56 -0400

    adding names to the code

--- Demo.java
+++ Demo.java
@@ -3,27 +3,56 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+
+class Student implements Comparable<Student>
+{
+    int age;
+    String name;
+
+    
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public int compareTo(Student that) {
+        return 0;
+    }
+
+    
+
+}
+
+
+
+
+
 public class Demo {
     public static void main(String[] args) throws InterruptedException
     {
 
-        Comparator<Integer> com = new Comparator<Integer>() {
-            public int compare(Integer i, Integer j)
+        Comparator<Student> com = new Comparator<Student>() {
+            public int compare(Student i, Student j)
             {
-                if(i%10 > j%10)
+                if(i.age > j.age)
                     return 1;
                 else
                     return -1;
             }
         };
-       List<Integer> nums = new ArrayList<>();
-       nums.add(432);
-       nums.add(895);
-       nums.add(64);
-       nums.add(378954);
+       List<Student> studs = new ArrayList<>();
+       studs.add(new Student(25, "nick"));
+       studs.add(new Student(65, "nehanth"));
+       studs.add(new Student(13, "alex"));
+       studs.add(new Student(14, "tom"));
 
-       Collections.sort(nums, com);
-       System.out.println(nums);
+       Collections.sort(studs);
+       for(Student s : studs)
+       {
+        System.out.println(s);
+       }
     }
     
 }
