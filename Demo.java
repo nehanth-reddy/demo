70c952115979389691bc3df1bc5a090a39c12ba0
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 12:33:06 -0400

    using hash table instead of hashmap. cannot print the same key twice

--- Demo.java
+++ Demo.java
@@ -1,16 +1,23 @@
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 public class Demo {
     public static void main(String[] args) throws InterruptedException
     {
-       Map<String, Integer> students = new HashMap<>();
+       Map<String, Integer> students = new Hashtable<>();
 
        students.put("Navin", 56);
        students.put("Harsh", 23);
        students.put("Sushil", 67);
        students.put("Kiran", 92);
-       
-       System.out.println(students.get("Harsh"));
+       students.put("Kiran", 93);
+
+       System.out.println(students);
+
+       for(String key : students.keySet())
+       {
+        System.out.println(key + " : " + students.get(key));
+       }
     }
     
 }
