77da240d392d7829c8b12698359af1488e7dcaf6
nehanth-reddy <pnehanthd@gmail.com>
2024-06-18 11:14:01 -0400

    new method of collections

--- Demo.java
+++ Demo.java
@@ -1,43 +1,22 @@
-class Counter
-{
-    int Count;
-    public synchronized void increment()
-    {
-        Count++;
-    }
-}
+import java.util.Collection;
+import java.util.TreeSet;
 
 public class Demo {
     public static void main(String[] args) throws InterruptedException
     {
-        Counter c = new Counter();
-        Runnable obj1 = () ->
-        {
-            for(int i=1;i<=1000;i++)
-            {
-               c.increment();
-            }
-        };
-        Runnable obj2 = () ->
-        {
-            for(int i=1;i<=1000;i++)
-            {
-               c.increment();
-            }
-        };
+       Collection<Integer> nums = new TreeSet<Integer>();
+       nums.add(62); 
+       nums.add(54);
+       nums.add(82);
+       nums.add(21);
 
-        Thread t1 = new Thread(obj1);
-        Thread t2 = new Thread(obj2);
-        
-        t1.start();
-        t2.start();
 
-        t1.join();
-        t2.join();
-        System.out.println(c.Count);
+
+       for(int n : nums)
+       {
+            System.out.println(n);
+       }
     }
-    
-
     
 }
    
